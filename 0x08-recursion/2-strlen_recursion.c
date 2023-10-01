4cf9b6c5d0a316300cd3cf12965b832bb4d3ba67
wafa-omer <wafakafoot123@gmail.com>
2023-10-01 06:04:07 -0700

    DONE

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion -return the length of a string 
- * @s:pointer to string
- * return :int
+ *  _strlen_recursion - Returns Length of String
+ * @s: pointer to string
+ * Return : int
  */
 
 int _strlen_recursion(char *s)
 {
 	int ntr = 0;
-	if (*s > '\0')
+	if (*s  > '\0')
 	{
 	ntr += _strlen_recursion(s + 1) + 1;
 	}
