7f543d57c79b1d744a0aa09ab64e799e197d45bb
wafa-omer <wafakafoot123@gmail.com>
2023-10-10 16:32:24 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a dog
+ * @dog : the dog to print
+ *
+ * Return : void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+	printf("NAME: %s\n", d->name ? d->name : "(nil)");
+	printf("AGE: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
