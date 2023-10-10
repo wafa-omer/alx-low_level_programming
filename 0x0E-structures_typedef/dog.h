14d67490d381ece247c36235632ae177e7683445
wafa-omer <wafakafoot123@gmail.com>
2023-10-10 16:05:49 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog  - a dog struct
+ * @name : dog name
+ * @age : dog age
+ * @owner : owner name
+ *
+ * Description : just long struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
