7a8e9c21c25407facefd217529ba1f8ef0510dab
wafa-omer <wafakafoot123@gmail.com>
2023-10-12 07:37:10 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print number with separator
+ * @separator: the string separator
+ * @n: the number of arguments
+ * @...: the integers to print
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+	{
+	printf("\n");
+	return;
+	}
+	va_start(ap, n);
+	while (1--)
+		printf("%d%s", va_arg(ap, int),
+			! ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
